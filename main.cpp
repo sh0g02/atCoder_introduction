c5fbde670e2d69bb368842ef13eeba9027ecff5a
sh0g02 <sh0g0.tksk@gmail.com>
2023-09-03 22:25:24 +0900

    p91までdone

--- main.cpp
+++ main.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    int ans = 1;
-    while(ans * 2 <= n)
-    {
-        ans = ans * 2;
-        // ans <<= 1;: ans を2倍にするために、ビットを1つ左にシフトする。これは ans = ans * 2; と同等。
+
+    vector<int> a(n);
+
+//    for (int i = 0; i < n; ++i) {
+//        cin >> a[i];
+//    }
+
+    // &アンパサンドをつけて、参照変数にする
+    for (auto& _a : a) {
+        cin >> _a;
     }
-    cout << ans << endl;
+
+    int count = 0;
+
+    while (true) {
+        bool can_do = true;
+
+        // 範囲forを使ってこう書ける
+        for (auto _a : a) {
+            if (_a % 2 == 1) {
+                can_do = false;
+            }
+        }
+
+//        for (int i = 0; i < n; ++i) {
+//            if (a[i] % 2 == 1) {
+//                can_do = false;
+//            }
+//        }
+
+        if (!can_do) {
+            break;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            a[i] /= 2;
+        }
+
+        ++count;
+    }
+
+    cout << count << endl;
     return 0;
 }
